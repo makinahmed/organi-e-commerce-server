568b58aad3e671e9338644f7017e3a898bfd6167
makinahmed <ahmedmakin555@gmail.com>
2022-02-13 19:07:36 +0600

    developed feature and popular products api

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+const port = 5000;
+app.use(cors())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8c0v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("organi-shop-e-commerce");
+    const featureCollection = database.collection("feature");
+    const popularProductCollection = database.collection("popularProduct");
+
+    // GET Features
+
+    app.get("/features", async (req, res) => {
+      const cursor = featureCollection.find({});
+      const features = await cursor.toArray();
+      res.send(features);
+    });
+    app.get("/popularProduct", async (req, res) => {
+      const cursor = popularProductCollection.find({});
+      const features = await cursor.toArray();
+      res.send(features);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Organi E-commerce Shop");
+});
+
+app.listen(port, () => {
+  console.log("Listening on the ", port);
+});
